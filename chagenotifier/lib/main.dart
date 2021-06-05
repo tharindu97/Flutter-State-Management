9f528f5a2ea49e533ead76d1c0477bfb3f6d85a1
Tharindu97 <tharindukavishna@gmail.com>
2021-06-05 21:51:41 +0530

    change notifier part completed

--- chagenotifier/lib/main.dart
+++ chagenotifier/lib/main.dart
@@ -1,1 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (_) => Favorites(),
+    child: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title:
+              Text('My Favoriet is ' + Provider.of<Favorites>(context).fruit),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              FruitButton(fruit: 'Apples'),
+              FruitButton(fruit: 'Orange'),
+              FruitButton(fruit: 'bananas')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FruitButton extends StatelessWidget {
+  final String? fruit;
+  FruitButton({@required this.fruit});
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          Provider.of<Favorites>(context, listen: false)
+              .changeFruit(fruit ?? 'fruit');
+        },
+        child: Text(fruit ?? 'fruit'));
+  }
+}
+
+class Favorites extends ChangeNotifier {
+  String fruit = 'Unkonwn';
+  void changeFruit(String newFruit) {
+    fruit = newFruit;
+    notifyListeners();
+  }
+}
